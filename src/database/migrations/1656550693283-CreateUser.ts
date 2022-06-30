00bc643996708db4f270704b0827e1a173dba187
teixeirajr <teixeirajunior1710@gmail.com>
2022-06-29 23:43:58 -0300

    config file migrations

--- src/database/migrations/1656550693283-CreateUser.ts
+++ src/database/migrations/1656550693283-CreateUser.ts
@@ -3,6 +3,17 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class CreateUser1656550693283 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE construction.users (
+            id INT auto_increment NOT NULL,
+            name varchar(100) NOT NULL,
+            email varchar(100) NOT NULL,
+            password varchar(100) NOT NULL,
+            company varchar(100) NULL,
+            CONSTRAINT users_PK PRIMARY KEY (id)
+        )
+        ENGINE=InnoDB
+        DEFAULT CHARSET=latin1
+        COLLATE=latin1_swedish_ci`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
--- src/database/migrations/ormconfig.ts
+++ src/database/migrations/ormconfig.ts
@@ -1,15 +1,26 @@
 import path from 'path';
 import dotenv from 'dotenv';
+import { DataSource } from "typeorm"
 
 dotenv.config({
     path: path.resolve(__dirname, '../../.env')
 });
 
-export default {
-    type: process.env.DATABASE_TYPE,
-    host: process.env.DATABASE_HOST,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT,
-  };
+const AppDataSource = new DataSource({
+    type: 'mariadb',
+    host: 'localhost',
+    port: 3306,
+    username: 'construction',
+    password: 'construction',
+    database: 'construction',
+})
+
+AppDataSource.initialize()
+.then(() => {
+    console.log("Data Source has been initialized!")
+})
+.catch((err) => {
+    console.error("Error during Data Source initialization", err)
+})
+
+export default AppDataSource;
